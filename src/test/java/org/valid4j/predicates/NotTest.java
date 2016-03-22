5d234b8a861a6f035d1ebb453df42b720cdaa98a
Patrik Helsing <patrik.helsing@gmail.com>
2016-03-22 23:34:09 +0100

    Minor refactorings of NotTest

--- src/test/java/org/valid4j/predicates/NotTest.java
+++ src/test/java/org/valid4j/predicates/NotTest.java
@@ -3,32 +3,33 @@ package org.valid4j.predicates;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.valid4j.predicates.CorePredicates.equalTo;
-import static org.valid4j.predicates.CorePredicates.not;
+import static org.valid4j.predicates.CorePredicates.*;
+import static org.valid4j.predicates.Evaluation.evaluate;
 import static org.valid4j.predicates.helper.TestingPredicates.otherPred;
 
 public class NotTest {
 
   @Test
   public void shouldTestTrue() {
-    assertTrue(not(equalTo(4)).test(3));
-    assertTrue(not(equalTo((Number) 4)).test(3));
-    assertTrue(not(equalTo((Number) 4)).test(2.3));
-    assertTrue(not(equalTo(4)).test(null));
+    assertTrue(evaluate(3, not(equalTo(4))).result());
+    assertTrue(evaluate(3, not(equalTo((Number) 4))).result());
+    assertTrue(evaluate(2.3, not(equalTo((Number) 4))).result());
+    assertTrue(evaluate(null, not(equalTo(4))).result());
   }
 
   @Test
   public void shouldTestFalse() {
-    assertFalse(not(equalTo(4)).test(4));
+    assertFalse(evaluate(4, not(equalTo(4))).result());
   }
 
   @Test
   public void shouldBeDescriptive() {
     assertEquals("not (equal to '4')", not(equalTo(4)).toString());
+    assertEquals("not (instance of java.lang.Integer)", not(instanceOf(Integer.class)).toString());
   }
 
   @Test
   public void shouldDescribeType() {
-    assertEquals("describing(4)", not(otherPred("describing")).describe(4));
+    assertEquals("describing(4)", evaluate(4, not(otherPred("describing"))).actual());
   }
 }
